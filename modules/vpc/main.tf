a50cbb40a974d858ab147ca376dbd907661bf76d
Pattabiraman <pattabimanu@gmail.com>
2025-10-06 14:08:06 +0530

    vpc update

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -22,20 +22,24 @@ resource "aws_subnet" "private" {
   availability_zone = var.azs[count.index]
 }
 
-resource "aws_internet_gateway" "eks_ig" {
+resource "aws_internet_gateway" "eks_igw" {
   vpc_id = aws_vpc.eks_vpc.id
+  tags = {
+    Name = "${var.vpc_name}-igw"
+  }
 }
 
 resource "aws_eip" "nat" {
-  count = length(var.public_subnets)
-  domain = "vpc" 
-}   
+  count  = length(var.public_subnets)
+  domain = "vpc"
+}
 
 resource "aws_nat_gateway" "ngw" {
   count         = length(var.public_subnets)
   allocation_id = aws_eip.nat[count.index].id
   subnet_id     = aws_subnet.public[count.index].id
-  depends_on    = [aws_internet_gateway.eks_ig]
+  depends_on    = [aws_internet_gateway.eks_igw]
+}
 }
 
 resource "aws_route_table" "public_rt" {
@@ -45,7 +49,8 @@ resource "aws_route_table" "public_rt" {
 resource "aws_route" "public_rt_route" {
   route_table_id         = aws_route_table.public_rt.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.eks_ig.id
+  gateway_id             = aws_internet_gateway.eks_igw.id
+}
 }
 
 resource "aws_route_table_association" "public_rta" {
@@ -71,23 +76,3 @@ resource "aws_route_table_association" "private_rta" {
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private_rt[count.index].id
 }
-
-resource "aws_security_group" "eks_cluster_sg" {
-  name        = "${var.vpc_name}-eks-cluster-sg"
-  description = "EKS Cluster Security Group"
-  vpc_id      = aws_vpc.eks_vpc.id
-
-  ingress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    self      = true
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-} 
