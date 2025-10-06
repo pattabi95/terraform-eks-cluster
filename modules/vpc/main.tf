bcd9f192a0785ce1b78da6f8dbefec3e0ea82749
Pattabiraman <pattabimanu@gmail.com>
2025-10-06 14:09:00 +0530

    vpc update

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -40,7 +40,7 @@ resource "aws_nat_gateway" "ngw" {
   subnet_id     = aws_subnet.public[count.index].id
   depends_on    = [aws_internet_gateway.eks_igw]
 }
-}
+
 
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.eks_vpc.id
@@ -51,7 +51,7 @@ resource "aws_route" "public_rt_route" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.eks_igw.id
 }
-}
+
 
 resource "aws_route_table_association" "public_rta" {
   count          = length(var.public_subnets)
@@ -75,4 +75,4 @@ resource "aws_route_table_association" "private_rta" {
   count          = length(var.private_subnets)
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private_rt[count.index].id
-}
+} 
