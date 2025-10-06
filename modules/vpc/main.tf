010d3441138968819b080558438d8eb43d4af220
Pattabiraman <pattabimanu@gmail.com>
2025-10-06 14:41:24 +0530

    vpc update

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -75,4 +75,4 @@ resource "aws_route_table_association" "private_rta" {
   count          = length(var.private_subnets)
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private_rt[count.index].id
-} 
+}
